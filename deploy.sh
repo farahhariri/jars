6d659f0203c56f907b9e1b0d45b5bbc627028ddd
farahhariri <elhariri.farah@gmail.com>
2016-11-21 20:26:51 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ cd ..
 rm -rf out/**/* || exit 0
 
 # Run our compile script
-doCompile
+mvn clean install
 
 # Now let's go have some fun with the cloned repo
 cd out
@@ -39,10 +39,10 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+#if [ -z `git diff --exit-code` ]; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
